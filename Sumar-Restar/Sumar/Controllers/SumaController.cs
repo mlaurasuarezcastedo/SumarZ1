cec819f09f5a0facd46de9b0003f944f059ef890
marialaurasc311 <marialaurasc311@gmail.com>
2020-08-15 09:37:19 -0400

    Se agrego Multiply

--- Sumar-Restar/Sumar/Controllers/SumaController.cs
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -12,13 +12,29 @@ namespace Sumar.Controllers
     public class SumaController : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add(int a, int b){
             return a + b;
         }
+
         [HttpPost]
         public int Add2([FromHeader]int a, [FromHeader]int b)
         {
             return a + b;
+        }
+
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mul2([FromHeader] int a, [FromHeader] int b)
+        {
+            return a * b;
         }
     }
 }
