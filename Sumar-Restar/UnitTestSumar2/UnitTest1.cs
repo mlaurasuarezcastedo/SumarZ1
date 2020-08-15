dd066463a92443174f99a56ca70a9fdebc63d548
marialaurasc311 <marialaurasc311@gmail.com>
2020-08-15 11:34:12 -0400

    Mi primer UnitTest

--- Sumar-Restar/UnitTestSumar2/UnitTest1.cs
+++ Sumar-Restar/UnitTestSumar2/UnitTest1.cs
@@ -1,26 +1,24 @@
-using NUnit.Framework;
-namespace UnitTestSumar
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSumar2
 {
     [TestClass]
     public class UnitTest1
     {
-        public object Assert { get; private set; }
-
         [TestMethod]
-            public void TestAdd1()
-            {
-                //Arrange
+        public void TestMethod1()
+        {
+            //Arrange
             SumaController sumaController = new SumaController();
             int a = 5;
             int b = 8;
             int esperado = 13;
 
-                //Act
+            //Act
             int resultado sumaController.Add(a, b);
 
             //Assert
             Assert.AreEqual(esperado, resultado);
-
-            }
+        }
     }
 }
